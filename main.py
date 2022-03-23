b852530a5be0752f5fad9ca7ce849fc01d0de4b5
Dhruv <lambadhruv09@gmail.com>
2022-03-23 14:41:08 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy
 import tflearn
 import random
 import json
+import tensorflow
 
 with open("intents.json") as file:
   data=json.load(file)
@@ -17,23 +18,23 @@ docs_y=[]
 docs_x=[]
 
 for intent in data["intents"]:
-  for pattern in intent["patters"]:
+  for pattern in intent["patterns"]:
     wrds=nltk.word_tokenize(pattern)
     words.extend(wrds)
-    docs_x.append(pattern)
+    docs_x.append(wrds)
     docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
       labels.append(intent["tags"])
 
-words=[stemmer.stem(w.lower()) for w in words]
+words=[stemmer.stem(w.lower()) for w in words if w !="?"]
 words=sorted(list(set(words)))
 
 labels=sorted(labels)
 training=[]
 output=[]
 
-out_empty=[0 for _ in range(len(classes))]
+out_empty=[0 for _ in range(len(labels))]
 for x, doc in enumerate(docs_x):
   bag=[]
   wrds=[stemmer.stem(w) for w in doc]
@@ -52,3 +53,17 @@ for x, doc in enumerate(docs_x):
 
 training=numpy.array(training)
 output= numpy.array(output)
+
+training=numpy.array(training)
+output=numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net=tflearn.input_data(shape=[None,len(training[0])])
+net=tflearn.fully_connected(net,8)
+net=tflearn.fully_connected(net,8)
+net=tflearn.fully_connected(net,len(output[0]),activation="softmax")
+net=tflearn.regression(net)
+model=tflearn.DNN(net)
+model.fit(training,output,n_epoch=100,box_size=8,show_metric=True)
+model.save("model.tfearn")
